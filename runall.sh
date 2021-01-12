749482856f285e59b77abd28ecf22a840a41de91
Ziad Ganim <ziadga@gmail.com>
2021-01-12 15:49:40 -0500

    data run

--- runall.sh
+++ runall.sh
@@ -1,6 +1,7 @@
 rm *.png
 find /gpfs/scratch60/ganim/zg54/aerys/2020-12-15/ -mindepth 1 -maxdepth 1 -type d -exec echo {} \;
-find /gpfs/scratch60/ganim/zg54/aerys/2020-12-15/ -mindepth 1 -maxdepth 1 -type d -exec python interf_plot.py --dir {}/ \;
+#find /gpfs/scratch60/ganim/zg54/aerys/2020-12-15/ -mindepth 1 -maxdepth 1 -type d -exec python interf_plot.py --dir {}/ \;
+find /gpfs/scratch60/ganim/zg54/aerys/2020-12-15/ -mindepth 1 -maxdepth 1 -type d -exec python interf_plot.py --dir {}/ --loadref --refname out12.txt \;
 git add .
 git commit -am "data run"
 git push
